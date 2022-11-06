1bad1d6ed4835d4eee44b52b4a88aa9878ca7ddc
kynikov <hellboynikov@gmail.com>
2022-11-06 12:33:47 +0200

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые'
+  ]
+  const onClickCategory = (i) => {
+    setActiveIndex(i);
+  }
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Categories;
